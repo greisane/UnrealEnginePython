3e3ebff1e49bde62f54f35ba5b1a2fd897b116c1
greisane <ggreisane@gmail.com>
2020-04-08 12:13:10 -0300

    Remove Python home discovery from build script, always use included

--- Source/UnrealEnginePython/Public/UnrealEnginePython.h
+++ Source/UnrealEnginePython/Public/UnrealEnginePython.h
@@ -16,14 +16,14 @@
 #include "Runtime/Launch/Resources/Version.h"
 
 #if PLATFORM_MAC
-#include <Headers/Python.h>
-#include <Headers/structmember.h>
+#include <include/Python.h>
+#include <include/structmember.h>
 #elif PLATFORM_LINUX
-#include <Python.h>
-#include <structmember.h>
+#include <include/Python.h>
+#include <include/structmember.h>
 #elif PLATFORM_ANDROID
-#include <Python.h>
-#include <structmember.h>
+#include <include/Python.h>
+#include <include/structmember.h>
 #elif PLATFORM_WINDOWS
 #include <include/pyconfig.h>
 #ifndef SIZEOF_PID_T
